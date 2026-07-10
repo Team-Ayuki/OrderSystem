1dded8a906af1dc892c2bd3bd75e533700df886c
Yuto Okamura <c5064@oic.jp>
2026-07-10 13:18:09 +0900

    MainWindowバグ修正

--- OrderSystem/View/MainWindow.xaml.cs
+++ OrderSystem/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace OrderSystem
+namespace OrderSystem.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
