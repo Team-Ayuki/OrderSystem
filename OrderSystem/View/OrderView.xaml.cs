0856e912f57acf84ac124c2ca9bd719fe2bff3e7
Yuto Okamura <c5064@oic.jp>
2026-07-10 13:05:13 +0900

    View,ViewModel諸調整

--- OrderSystem/View/OrderView.xaml.cs
+++ OrderSystem/View/OrderView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace OrderSystem
+namespace OrderSystem.View
 {
     /// <summary>
     /// OrderView.xaml の相互作用ロジック
